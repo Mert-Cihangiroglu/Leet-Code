9e165c8c30fb827ab0d49aafc11eca58f099bc70
Mert <mert@Merts-MacBook-Pro.local>
2022-11-17 18:24:46 +0100

    BestTimeToBuyStock

---
+++ TimetoBuyandSellStock.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        l, r = 0, 1 # Left and Right Pointers, Left = Buy, Right = Sell
+        max_p = 0   # Max Profit
+        
+        while r < len(prices):
+            # profitable?
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                # if profit is bigger than current max profit assign it as new max_p
+                max_p = max(max_p, profit)
+            else:
+                l = r
+            r += 1
+            
+        return max_p
