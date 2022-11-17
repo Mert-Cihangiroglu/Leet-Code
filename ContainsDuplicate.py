ef1c7f0e7130299027841e9e46a8ef40ee4f655a
Mert <mert@Merts-MacBook-Pro.local>
2022-11-17 18:32:10 +0100

    ContainsDuplicate

---
+++ ContainsDuplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Put everything we iterate in a set
+        # If the current value is in the set, return True
+        # If the asked value never appears in the set, Return False.
+        hashset = set()
+        # Loop through the list
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
